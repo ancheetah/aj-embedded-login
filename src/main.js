5fad7f74d7e4bb6a0269b278f678a09c29b155df
AJ Ancheta <7781450+ancheetah@users.noreply.github.com>
2025-02-06 18:31:35 -0500

    config the js sdk

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
+import { Config } from '@forgerock/javascript-sdk';
+
+Config.set({
+    clientId: 'aj-public-sdk-client', // e.g. 'ForgeRockSDKClient'
+    // redirectUri: `${window.location.origin}/callback.html`, // e.g. 'https://sdkapp.example.com:8443/callback.html'
+    // scope: 'openid profile email address', // e.g. 'openid profile email address phone'
+    serverConfig: {
+        baseUrl: 'https://openam-sdks.forgeblocks.com/am', // e.g. 'https://myorg.forgeblocks.com/am' or 'https://openam.example.com:8443/openam'
+        timeout: parseInt(60000), // 90000 or less
+    },
+    realmPath: 'alpha', // e.g. 'alpha' or 'root'
+    // tree: process.env.TREE, // e.g. 'sdkAuthenticationTree' or 'Login'
+});
+
 function component() {
     const el = document.createElement('div');
     el.innerHTML = 'Hello World';
